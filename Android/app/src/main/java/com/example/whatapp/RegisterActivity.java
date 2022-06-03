832d5dd6b854a0c2029248065d643b83a0570623
adi604 <84729170+adi604@users.noreply.github.com>
2022-06-03 13:35:57 +0300

    update register activity

--- Android/app/src/main/java/com/example/whatapp/RegisterActivity.java
+++ Android/app/src/main/java/com/example/whatapp/RegisterActivity.java
@@ -2,7 +2,9 @@ package com.example.whatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Button btnSignIn = findViewById(R.id.btnSignIn);
+        btnSignIn.setOnClickListener(v -> {
+            Intent i = new Intent(this, MainActivity.class);
+            startActivity(i);
+        });
     }
 }
